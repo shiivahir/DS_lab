3448dfa4bc2d074af9903a3e181cac22525d0eb7
Shiv Ahir <shiivahir@gmail.com>
2026-07-30 01:02:49 +0530

    Added practical 5A

---
+++ practical_5A/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practical_5A
+//
+//  Created by apexpredator on 30/07/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
