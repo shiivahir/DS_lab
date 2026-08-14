ec328dcfece97dc04f8eae8aed0bc82a7e5af2c2
Shiv Ahir <shiivahir@gmail.com>
2026-08-14 12:15:30 +0530

    Practical 4 and 5

---
+++ practical_4B/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practical_4B
+//
+//  Created by apexpredator on 14/08/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
